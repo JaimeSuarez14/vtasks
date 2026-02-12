45a439ed45ed04c0a040687e64e68587622c3251
Jaime Suarez <jaisua8@gmail.com>
2026-02-11 22:44:23 -0500

    creacion del proyecto fastapi

---
+++ api.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def saludar():
+  return { "saludo": "Hola mundo desde fastapi" }
+
+@app.get('/post/{id}')
+def add(id: int):
+  return{ "id" : id}
