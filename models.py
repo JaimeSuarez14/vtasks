a07312fcab848bf93ec86bd2bcfcbb0076239d95
Jaime Suarez <jaisua8@gmail.com>
2026-02-12 22:19:45 -0500

    validacion de los campos por metodos

--- models.py
+++ models.py
@@ -1,21 +1,26 @@
 from enum import Enum
 from typing import Annotated
-from pydantic import BaseModel, Field, StringConstraints, field_validator
+from pydantic import AfterValidator, BaseModel, Field, StringConstraints, field_validator
 
 
 class StatusType(str, Enum):
   DONE = "hecho"
   PENDING = "pending"
 
+def espacios(value: str) -> str:
+    if value.strip()== "":
+        raise ValueError(f'{value} is not an even number')
+    return value 
+
 class Task(BaseModel):
   id:int =  Field(gt=0)
-  name: str  = Field(min_length=3, strip_whitespace=True)
-  description:  str = Field(min_length=3)
+  name:  Annotated[str, AfterValidator(espacios)]  = Field(min_length=3)
+  description:   Annotated[str, AfterValidator(espacios)] = Field(min_length=3)
   status: StatusType 
 
-  @field_validator('description', mode='after')  
-  @classmethod
-  def is_even(cls, value: str) -> str:
-        if value.strip()==0 :
-            raise ValueError(f'{value} nos debe contener espacios vacios')
-        return value  
+  #@field_validator('description', mode='after')
+  #@classmethod
+  #def is_even(cls, value: str) -> str:
+  #      if value.strip()== "" :
+  #          raise ValueError(f'La description no debe contener espacios vacios')
+  #      return value  
